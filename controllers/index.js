9c7415acce14df44bd49b07026cd18e5d26a28a7
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-09-05 10:56:11 +0300

    fix: this fix updating models routes

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,7 @@ const {
   getUser,
   deleteUser,
   updateUser,
+  getProfile,
 } = require("./users/usersController");
 
 module.exports = {
@@ -12,4 +13,5 @@ module.exports = {
   deleteUser,
   updateUser,
   getUser,
+  getProfile,
 };
--- services/DB.js
+++ services/DB.js
@@ -16,33 +16,38 @@ class DB {
     return await User.findByPk(id, { include: Profile });
   }
 
-  static async getProfileById(id) {
+  static async getProfile(id) {
     return await Profile.findByPk(id);
   }
 
   static async editUser(data, id) {
-    const updatedProfile = await Profile.update(data, {
+    const findUser = await User.findByPk(id);
+    if (!findUser?.ProfileId) return null;
+    const user = await User.update(data, {
       where: { id },
       returning: true,
     });
 
-    const updatedUser = await User.update(data, {
-      where: { id },
+    const profile = await Profile.update(data, {
+      where: { id: findUser.ProfileId },
       returning: true,
     });
 
-    return {
-      updatedProfile: updatedProfile[1],
-      updatedUser: updatedUser[1],
-    };
+    return { user: user[1], profile: profile[1] };
   }
 
   static async deleteUser(id) {
-    return await User.destroy({ where: { id } });
+    const findUser = await User.findByPk(id);
+    if (!findUser) return null;
+    return await Profile.destroy({
+      where: { id: findUser.ProfileId },
+    });
   }
 
   static async deleteProfile(id) {
-    return await Profile.destroy({ where: { id } });
+    return await Profile.destroy({
+      where: { id },
+    });
   }
 }
 
